b3fbb3b89a8960db1697f54d46127919ce9f3bd2
mutakin-san <mutakin.email@gmail.com>
2021-12-12 21:10:37 +0700

    Code Optimization Change Layout Inflating with lateinit field/attribute

--- app/src/main/java/com/mutakin/diceroller/MainActivity.kt
+++ app/src/main/java/com/mutakin/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mutakin.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
@@ -11,12 +12,19 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnRoll: FloatingActionButton
+    private lateinit var diceImage: ImageView
+    private lateinit var resultText: TextView
+    private lateinit var animation: Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val btnRoll: FloatingActionButton = findViewById(R.id.btn_roll)
+        btnRoll = findViewById(R.id.btn_roll)
+        diceImage = findViewById(R.id.dice)
+        resultText = findViewById(R.id.result_text)
+        animation = AnimationUtils.loadAnimation(this, R.anim.dice_animation)
 
         btnRoll.setOnClickListener {
             rollDice()
@@ -24,16 +32,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val dice: ImageView = findViewById(R.id.dice)
         val randomInt = Random.nextInt(6) + 1
-
-        val animation = AnimationUtils.loadAnimation(this, R.anim.dice_animation)
-        dice.startAnimation(animation)
+        diceImage.startAnimation(animation)
 
         Handler().postDelayed({
-
-            var drawable: Int = when (randomInt) {
+            val drawable: Int = when (randomInt) {
                 1 -> R.drawable.dice_1
                 2 -> R.drawable.dice_2
                 3 -> R.drawable.dice_3
@@ -43,7 +46,7 @@ class MainActivity : AppCompatActivity() {
                 else -> R.drawable.empty_dice
             }
 
-            dice.setImageDrawable(resources.getDrawable(drawable, applicationContext.theme))
+            diceImage.setImageResource(drawable)
             resultText.text = "$randomInt"
         }, 800)
 
